eec757d9fbf7addabd7cff456fc4cb68d8b58400
Jess Brown <jess@brownwebdesign.com>
2012-01-18 15:15:39 -0500

    setup main layout

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,26 +1,10 @@
-/* Author: 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function(){
+    $(window).bind('orientationchange', function(event) {
+        if (window.orientation == 90 || window.orientation == -90 || window.orientation == 270) {
+            $('meta[name="viewport"]').attr('content', 'height=device-width,width=device-height,initial-scale=1.0,maximum-scale=1.0');
+        } else {
+            $('meta[name="viewport"]').attr('content', 'height=device-height,width=device-width,initial-scale=1.0,maximum-scale=1.0');
+        }
+    }).trigger('orientationchange');
+    
+});
